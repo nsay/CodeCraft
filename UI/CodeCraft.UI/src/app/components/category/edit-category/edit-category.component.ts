c34521b6d2ded87ef26da298c6bda57d795f89a0
nsay <narithsay@gmail.com>
2023-10-18 23:20:03 -0400

    create getCategoryById service; display edit category

--- UI/CodeCraft.UI/src/app/components/category/edit-category/edit-category.component.ts
+++ UI/CodeCraft.UI/src/app/components/category/edit-category/edit-category.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-edit-category',
@@ -11,21 +13,33 @@ export class EditCategoryComponent implements OnInit, OnDestroy {
 
     id: string | null = null;
     private idSubscription?: Subscription;
+    category?: Category;
 
-    constructor(private route: ActivatedRoute){
-
-    }
+    constructor(private route: ActivatedRoute, private categoryService: CategoryService){}
 
     //get the id from the URL ex: http://localhost:4200/admin/categories/{id}
     ngOnInit(): void {
         this.idSubscription = this.route.paramMap.subscribe({
             next: (params) => {
                 this.id = params.get('id');
+
+                //get data of this ID from API
+                if(this.id){
+                    this.categoryService.getCategoryById(this.id).subscribe({
+                        next: (response) => {
+                            this.category = response;
+                        }
+                    })
+                }
             }
         });
     }
 
     ngOnDestroy(): void {
         this.idSubscription?.unsubscribe();
+    }
+
+    submitForm(): void {
+        
     }
 }
